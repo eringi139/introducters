70f13f648806858e683c013d8ee7243f0f60db2a
EC2 Default User <ec2-user@ip-172-31-21-43.ap-northeast-1.compute.internal>
2024-03-24 04:22:00 +0000

    [Add] public新規登録画面レイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+scope module: :public do
+  post 'customers/guest_sign_in', to: 'customers/sessions#guest_sign_in'
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+end
 end
