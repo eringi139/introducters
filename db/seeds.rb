8e3e4bce84f86f9424857ed844b73995d7682876
EC2 Default User <ec2-user@ip-172-31-21-43.ap-northeast-1.compute.internal>
2024-05-04 12:20:28 +0000

    deploy setting

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,27 +28,32 @@ risa = Customer.find_or_create_by!(email: "ccc@ccc") do |customer|
   customer.password = "000000"
 end
 
+genre1 = Genre.create!(
+  name: "J-POP"
+)
+
+genre2 = Genre.create!(
+  name: "ボカロ"
+)
+
 Post.find_or_create_by!(title: "音楽A") do |post|
   post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
   post.body = "Aメロからサビにかけてのコードがお気に入りです。"
-  post.genre = "J-POP"
+  post.genre_id = genre1.id
   post.customer = sanae
 end
 
 Post.find_or_create_by!(title: "音楽B") do |post|
   post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
   post.body = "アップテンポな曲調で、聴くと元気がでます。 ドライブで流すのもおすすめです。"
-  post.genre = "J-POP"
+  post.genre_id = genre1.id
   post.customer = mizuki
 end
 
 Post.find_or_create_by!(title: "音楽C") do |post|
   post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
   post.body = "ボーカロイド楽曲はテンポが速いものがよく流行るイメージですが、ゆったりとした曲調で心が落ち着くボカロも素晴らしいということを多くの人に知ってもらいたいです。"
-  post.genre = "ボカロ"
+  post.genre_id = genre2.id
   post.customer = risa
 end
 
-genre = Genre.create!(
-  name: ""
-)
